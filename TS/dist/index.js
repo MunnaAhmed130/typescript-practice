a9bbbdf47552d9eb7c6c8a9210fb28ed7f437b7b
Munna Ahmed <munnaahmed2025@gmail.com>
2023-11-26 17:34:13 +0600

    class & access modifiers

--- TS/dist/index.js
+++ TS/dist/index.js
@@ -1,3 +1,47 @@
 "use strict";
-console.log("typescript is here");
-console.log("Hey");
+// class
+class User {
+    constructor(email, name) {
+        this.city = ""; // private in js
+        // city: string = "";
+        this.user = "user";
+        // types for this.property will be declared outside of constructor
+        this.email = email;
+        this.name = name;
+    }
+}
+// better way
+class NewUser {
+    constructor(email, name // private userId: string
+    ) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.user = "user";
+    }
+    // private method
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `{this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    // there should be no return type for setter
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+const adam = new User("email", "adam");
+const adam1 = new NewUser("email", "adam1");
+// error
+// const user1 = new User("email", 1);
+// adam.city
+// adam.user = "admin";
+// adam1.deleteToken() // this is only accessible within the Class
+console.log(adam, adam1);
--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,2 +1,55 @@
-console.log("typescript is here");
-console.log("Hey");
+// class
+class User {
+    // private, public keywords are access modifiers
+    // every propery is automatically public
+    public email: string;
+    // private properties not accessible to the instances of a class
+    private name: string; // private in ts
+    private city: string = ""; // private in js
+
+    // city: string = "";
+    readonly user: string = "user";
+    constructor(email: string, name: string) {
+        // types for this.property will be declared outside of constructor
+        this.email = email;
+        this.name = name;
+    }
+}
+
+// better way
+class NewUser {
+    private _courseCount: number = 1;
+    readonly user: string = "user";
+    constructor(
+        public email: string,
+        private name: string // private userId: string
+    ) {}
+    // private method
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail(): string {
+        return `{this.email}`;
+    }
+    get courseCount(): number {
+        return this._courseCount;
+    }
+    // there should be no return type for setter
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+
+const adam = new User("email", "adam");
+const adam1 = new NewUser("email", "adam1");
+
+// error
+// const user1 = new User("email", 1);
+// adam.city
+// adam.user = "admin";
+// adam1.deleteToken() // this is only accessible within the Class
+
+console.log(adam, adam1);
